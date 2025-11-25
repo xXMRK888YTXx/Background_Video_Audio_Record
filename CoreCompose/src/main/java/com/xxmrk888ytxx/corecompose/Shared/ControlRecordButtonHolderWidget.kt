7c76c1e78044173648c0d302b8d6e10e445dc8cb
xXMRK888YTXx <aksehchikov@gmail.com>
2025-11-25 09:24:32 +0300

    [Refactor] Update ControlRecordButtonHolderWidget
    Updated `ControlRecordButtonHolderWidget` to use `FlowRow` for better alignment and arrangement of its content. This change replaces the previous layout implementation.

--- CoreCompose/src/main/java/com/xxmrk888ytxx/corecompose/Shared/ControlRecordButtonHolderWidget.kt
+++ CoreCompose/src/main/java/com/xxmrk888ytxx/corecompose/Shared/ControlRecordButtonHolderWidget.kt
@@ -3,15 +3,14 @@ package com.xxmrk888ytxx.corecompose.Shared
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.unit.dp
 import com.xxmrk888ytxx.corecompose.themeColors
 import com.xxmrk888ytxx.corecompose.themeShapes
@@ -20,7 +19,7 @@ import com.xxmrk888ytxx.corecompose.themeShapes
 @Composable
 fun ControlRecordButtonHolderWidget(
     modifier: Modifier = Modifier,
-    content:@Composable RowScope.() -> Unit
+    content: @Composable RowScope.() -> Unit
 ) {
     Card(
         modifier = Modifier
@@ -31,12 +30,12 @@ fun ControlRecordButtonHolderWidget(
         backgroundColor = themeColors.cardColor
     ) {
         FlowRow(
-            Modifier
+            modifier = Modifier
                 .fillMaxWidth()
                 .padding(15.dp),
-            verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(15.dp, Alignment.CenterHorizontally),
-            content = content
+            content = content,
+            verticalArrangement = Arrangement.Center,
         )
     }
 }
